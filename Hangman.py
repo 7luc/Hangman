6f72ef359716c80582c28f057bd04ec86038e708
Lucas <Faisal45@me.com>
2026-07-06 02:24:00 +0300

    Update Hangman.py
    Implement hidden word display

--- Hangman.py
+++ Hangman.py
@@ -1,17 +1,22 @@
 from random import choice as ch
-animal = ["cat","dog","lion","tiger","elephant","giraffe","zebra","monkey","rabbit","bear"]
-countrie = ["japan","canada","brazil","egypt","italy","france","mexico","spain","india","turkey"]
-sport = ["football","tennis","cricket","volleyball","baseball","swimming","boxing","cycling","golf","karate"]
+animals = ["cat","dog","lion","tiger","elephant","giraffe","zebra","monkey","rabbit","bear"]
+countries = ["japan","canada","brazil","egypt","italy","france","mexico","spain","india","turkey"]
+sports = ["football","tennis","cricket","volleyball","baseball","swimming","boxing","cycling","golf","karate"]
 programming = ["python","java","kotlin","swift","golang","ruby","rust","html","css","sql"]
 food = ["pizza","burger","pasta","sushi","shawarma","falafel","steak","salad","noodles","sandwich"]
-movie = ["avatar","inception","gladiator","interstellar","coco","up","frozen","moana","titanic","joker"]
-categories = [animal,countrie,sport,programming,food,movie]
+movies = ["avatar","inception","gladiator","interstellar","coco","up","frozen","moana","titanic","joker"]
+categories = [animals,countries,sports,programming,food,movies]
 categoriesName = ["Animal","countrie","sport","programming","food","movie"]
 def word_choice():
     place = categories.index(ch(categories))
-    categorie = categories[place]
-    word = ch(categorie)
+    category = categories[place]
+    word = ch(category)
     print("Your Categorie is:"+str(categoriesName[place]))
-    print(word)
 
-word_choice()
+def display_word(guessedLetters,word):
+    for letter in word:
+        if letter in guessedLetters:
+            print(letter, end=" ")
+        else:
+            print("-", end=" ")
+    print("")
