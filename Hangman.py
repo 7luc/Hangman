796715f8c8a35221965bad17fd52065c22817eee
Lucas <Faisal45@me.com>
2026-07-06 19:31:50 +0300

    Complete Hangman game

--- Hangman.py
+++ Hangman.py
@@ -6,8 +6,9 @@ programming = ["python","java","kotlin","swift","golang","ruby","rust","html","c
 food = ["pizza","burger","pasta","sushi","shawarma","falafel","steak","salad","noodles","sandwich"]
 movies = ["avatar","inception","gladiator","interstellar","coco","up","frozen","moana","titanic","joker"]
 categories = [animals,countries,sports,programming,food,movies]
-category_names = ["Animal","countrie","sport","programming","food","movie"]
-def choose_letter():
+category_names = ["Animal","Countries","sport","programming","food","movie"]
+
+def choose_word():
     place = categories.index(ch(categories))
     category = categories[place]
     word = ch(category)
@@ -22,17 +23,118 @@ def display_word(guessed_letters,word):
             print("-", end=" ")
     print("")
 
-def choised_letter(letter):
+def check_allowed(letter):
+    if len(letter)==1 and letter.isalpha():
+        add_guessed_letter(letter)
+    elif len(letter)!=1:
+        print("Please enter only one letter")
+    else:
+        print("Please enter a letter only")
+
+def add_guessed_letter(letter):
+    global mistakes
     if letter in guessed_letters:
-        print("Doublicted letter!!")
+        print("You've already guessed this letter!!")
     else:
         guessed_letters.append(letter)
-        display_word(guessed_letters,word)
+        mistakes = check_winner(letter,word,mistakes)
 
-word, category_name = choose_letter()
-print("The category is:",str(category_name))
+def draw_hangman(mistakes):
+    hangman = [
+"""
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+
+"""
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+
+"""
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+
+"""
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+""",
+
+"""
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+""",
+
+"""
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========
+""",
+
+"""
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========
+"""
+]
+    print(hangman[mistakes])
+
+def check_winner(letter,word,mistakes):            
+    if letter not in word:
+        mistakes+=1
+        draw_hangman(mistakes)
+    return mistakes
+
+mistakes = 0
+word, category_name = choose_word()
+print("The category is:",category_name)
 guessed_letters = []
 while True:
+    if mistakes == 6:
+        print("Game Over! 💀")
+        print("The word was:"+word)
+        break
+    count=0
+    for letter in word:
+        if letter in guessed_letters:
+            count+=1
+    if count==len(word):
+        print("Congratulations! You guessed the word! 🎉")
+        break 
     display_word(guessed_letters,word)
     choice=input("Enter a letter:")
-    choised_letter(choice)
+    check_allowed(choice.lower())
